699d1276138169c94178a38315d8cd4cf6e118c9
atiwary17 <142500677+atiwary17@users.noreply.github.com>
2023-09-30 23:20:43 +0400

    Update video_guide.py

--- scipts/video_guide.py
+++ scipts/video_guide.py
@@ -1,8 +1,9 @@
-from IPython.display import HTML
-
-# Create an HTML iframe element to embed the YouTube video
-html_code = f"""
-<iframe width="800" height="450" src="https://www.youtube.com/embed/EJL3cIGQMz0?si=rveyz9SSYs56jp48" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"""
-
-# Display the embedded YouTube video
-HTML(html_code)
+def get_video_guide():
+  from IPython.display import HTML
+  # Create an HTML iframe element to embed the YouTube video
+  html_code = f"""
+  <iframe width="800" height="450" src="https://www.youtube.com/embed/EJL3cIGQMz0?si=rveyz9SSYs56jp48" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"""
+  
+  # Display the embedded YouTube video
+  HTML(html_code)
+  

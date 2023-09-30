ad9a80c4a9db970b02c61acc3c406bc03f843f76
atiwary17 <142500677+atiwary17@users.noreply.github.com>
2023-09-30 21:56:49 +0400

    Create video_guide.py

---
+++ scipts/video_guide.py
@@ -0,0 +1,8 @@
+from IPython.display import HTML
+
+# Create an HTML iframe element to embed the YouTube video
+html_code = f"""
+<iframe width="800" height="450" src="https://www.youtube.com/embed/EJL3cIGQMz0?si=rveyz9SSYs56jp48" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"""
+
+# Display the embedded YouTube video
+HTML(html_code)
